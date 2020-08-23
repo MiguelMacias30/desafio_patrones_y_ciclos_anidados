34c32d650298bc3040cda85b44d16c93c7039f02
Miguel Macias <miguelfernandomacias@gmail.com>
2020-08-22 23:44:29 -0400

    cuadrado

--- patrones.rb
+++ patrones.rb
@@ -1,3 +1,4 @@
+#letra I 
 n = ARGV[0].to_i
 
 n.times do
@@ -8,6 +9,27 @@ print "\n"
 (n -2).times do
   print " "
   (n -4).times do
+    print " "
+  end
+  print "*"
+  print "\n"
+end
+
+n.times do
+  print "*"
+end
+
+#cuadrado
+n = ARGV[0].to_i
+
+n.times do
+  print "*"
+end
+print "\n"
+
+(n - 2).times do
+  print "*"
+  (n - 2).times do
     print " "
   end
   print "*"
